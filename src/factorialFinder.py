789f0cbaefcdc5adaac2965977db0577a2446c28
oliver bonhamcarter <obonhamcarter@allegheny.edu>
2018-09-06 20:48:21 -0400

    Starter files for practical 2

---
+++ src/factorialFinder.py
@@ -0,0 +1,24 @@
+# calculating the factorial
+# getting the user-inputted number
+# tell user what number they entered
+# testing if the number is odd or even
+#!/usr/bin/env python3
+#AuthorEmail = "obonhamcarter@allegheny.edu"
+#Date = "6 Sept 2018"
+#OriginalAuthor = "Oliver Bonham-Carter"
+#Version = "i"
+else:
+fact_int = 1
+fact_int = fact_int * i
+for i in range(1,num_int+1):
+if num_int % 2 == 0:
+num_int = int(num_str) # convert to int
+num_str = input("\n Enter a number : ") # get input
+print ("  Current value of fact_int : ",fact_int)
+print("  The number <<",num_int,">> is EVEN: ")
+print("  The number <<",num_int,">> is ODD: ")
+print("  The number you entered is the following :",num_int)
+print("\n  * Factorial for",num_int,"is :", fact_int, "\n")
+print("\n Determining the factorial of the number ...")
+print("\n Testing the number for Odd or Even polarity ...")
+
